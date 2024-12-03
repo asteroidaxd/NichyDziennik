6052158d26070a60149248eb40ed1a20cab9e6ff
mariusz-rychlik <mariusz.rychlik.06@gmail.com>
2024-12-03 07:27:07 +0100

    add basic functions of interface

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿namespace dziennik
+{
+    internal class Program
+    {
+        static string wybor;
+        static bool poprawny = false;
+        static void Main(string[] args)
+        {
+            string wybor;
+            bool poprawny = false;
+
+            wyswietlOceny();
+          
+
+        }
+        static void wyswietlOceny()
+        {
+            Console.Clear();
+            Console.WriteLine("oceny: ");
+            Console.WriteLine("matematyka(3,8): 4,3,3,3");
+            Console.WriteLine("polski(1,1): 1,1,1,1,2");
+            Console.WriteLine("wf(6,0): 6");
+            Console.WriteLine("\nWpisz \"max\" lub \"min\" aby zobaczyc takie oceny z kazdego przedmiotu");
+            Console.WriteLine("wpisz \"e\" aby dodac lub usunac ocene lub przedmiot");
+            wybor = Console.ReadLine();
+            Console.Clear();
+
+            while (poprawny == false)
+            {
+                if (wybor == "e") { Console.Clear(); edycja(); poprawny = true; }
+                else if (wybor == "max") { Console.Clear(); maxOcena(); poprawny = true; }
+                else if (wybor == "min") { Console.Clear(); minOcena(); poprawny = true; }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nWpisz \"max\" lub \"min\" aby zobaczyc oceny");
+                    Console.WriteLine("wpisz \"E\" aby dodac lub usunac ocene lub przedmiot");
+                    wybor = Console.ReadLine();
+                }
+            }
+            poprawny = false;
+        }
+        static void edycja()
+        {
+            Console.Clear();
+            Console.WriteLine("Co chesz zmodyfikowac: ");
+            Console.WriteLine("oceny (o(+/-))     przedmioty (p(+/-))         przyklad:\"p-\"");
+            wybor = Console.ReadLine();
+            Console.Clear();
+            while (poprawny == false)
+            {
+                if (wybor == "o+") { Console.Clear(); dodajOcene(); poprawny = true; }
+                else if (wybor == "o-") { Console.Clear(); usunOcene(); poprawny = true; }
+                else if (wybor == "p+") { Console.Clear(); dodajPrzedmiot(); poprawny = true; }
+                else if (wybor == "p-") { Console.Clear(); usunPrzedmiot(); poprawny = true; }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Co chesz zmodyfikowac: ");
+                    Console.WriteLine("oceny (O(+/-))     przedmioty (P(+/-))         przyklad:\"P-\"");
+                    wybor = Console.ReadLine();
+                }
+            }
+            poprawny = false;
+        }
+        static void dodajOcene()
+        {
+            Console.WriteLine("Wpisz: przedmiot,ocena");
+            Console.WriteLine("przedmioty w dzienniku:");
+            Console.WriteLine("matematyka,polski,wf");
+            zapisz();
+        }
+        static void usunOcene()
+        {
+            Console.WriteLine("Wpisz: przedmiot,ocena,numer oceny liczac od 1 od najnowszych do najstarszych");
+            Console.WriteLine("przedmioty w dzienniku:");
+            Console.WriteLine("matematyka,polski,wf");
+            zapisz();
+        }
+        
+       static void dodajPrzedmiot()
+        {
+            Console.WriteLine("wpisz nazwe przedmiotu do dodania:");
+            zapisz();
+        }
+         static void usunPrzedmiot()
+        {
+            Console.WriteLine("wpisz nazwe przedmiotu do usuniecia:");
+            zapisz();
+        }
+
+         static void minOcena()
+        {
+            Console.WriteLine("matematyka: 1");
+            Console.WriteLine("polski: 2");
+            Console.WriteLine("wf: 1");
+            Console.WriteLine("\nnacisnij enter aby wrocic");
+            Console.ReadLine();
+            wyswietlOceny();
+        }
+         static void maxOcena()
+        {
+            Console.WriteLine("matematyka: 5");
+            Console.WriteLine("polski: 2");
+            Console.WriteLine("wf: 6");
+            Console.WriteLine("\nnacisnij enter aby wrocic");
+            Console.ReadLine();
+            wyswietlOceny();
+        }
+        static void zapisz()
+        {
+            Console.WriteLine("zapisano stan do pliku");
+        }
+    }
+    
+}
